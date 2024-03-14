b87c0f8cd1770f2b47920d06143c3f62444461a4
mandu <78725815+linelie00@users.noreply.github.com>
2024-03-14 14:19:18 +0900

    Create 2week1.c

---
+++ 2week1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int day[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    for(int i = 0; i < 12; i++) {
+        printf("%d %d\n", i+1, day[i]);
+    }
+    return 0;
+}
